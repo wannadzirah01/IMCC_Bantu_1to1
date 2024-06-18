e55a19dfc986c66edb62d497e6d2d9d6573fa19e
wannadzirah01 <wannadzirah.wa@gmail.com>
2024-06-18 18:39:18 +0800

    Update redis

--- backend/config.py
+++ backend/config.py
@@ -22,6 +22,9 @@ class ApplicationConfig:
     SESSION_TYPE = "redis"
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    # SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+    from redis import from_url
+
+    SESSION_REDIS = from_url("redis://red-cpom6b6ehbks73em1sqg:6379")
 
     
