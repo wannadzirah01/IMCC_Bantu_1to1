778dc68e328a7a23281a9429445f1b5220980e29
wannadzirah01 <wannadzirah.wa@gmail.com>
2024-06-18 15:54:00 +0800

    Updata DB config

--- backend/config.py
+++ backend/config.py
@@ -7,7 +7,15 @@ load_dotenv()
 class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
 
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/IMCC_Bantu_1To1'
+    DB_HOST = 'sql12.freesqldatabase.com'
+    DB_NAME = 'sql12714507'
+    DB_USER = 'sql12714507'
+    DB_PASS = 'SAij8Kfeha'
+    DB_PORT = '3306'
+    # Construct the SQLAlchemy database URI
+    SQLALCHEMY_DATABASE_URI = f"mysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+    # SQLALCHEMY_DATABASE_URI = 'mysql://root:''@localhost/IMCC_Bantu_1To1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
 
